f47dcbd874cd049c9dde16f5adbef872e7431db3
Vivek Kumar Gupta <100516335+thewisdomgupta@users.noreply.github.com>
2022-03-15 06:59:52 +0530

    Add files via upload

---
+++ Array/Smallest_Element_in_Array.java
@@ -0,0 +1,28 @@
+package javaallpackage;
+
+import java.util.Scanner;
+
+public class Smallest_Element_in_Array {
+    public static void main(String [] args){
+        System.out.println("Enter the Size of Array:");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println("Enter the Array Elements:");
+        int []arr=new int[10];
+        for(int i=0; i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("Array Elements are:");
+        for(int i=0; i<n; i++){
+            System.out.println(arr[i]);
+        }
+        int m=arr[0];
+        for(int i=0; i<n;i++){
+            if(m>arr[i]){
+                m=arr[i];
+            }
+        }
+        System.out.println("Smallest element in the array is:"+" "+m);
+    }
+}
+//This Code is Contributed by Vivek Kumar Gupta
