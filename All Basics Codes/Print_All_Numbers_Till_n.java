b1283d982b9ee78d487d147e474ced8c006ba6ac
Vivek Kumar Gupta <100516335+thewisdomgupta@users.noreply.github.com>
2022-03-27 13:20:11 +0530

    Add files via upload

---
+++ All Basics Codes/Print_All_Numbers_Till_n.java
@@ -0,0 +1,14 @@
+package javaallpackage;
+import java.util.Scanner;
+public class Print_All_Numbers_Till_n {
+    public static void main(String [] args){
+        System.out.println("Enter the nth Number:");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println("All the Numbers till"+" "+n+" "+"are:");
+        for(int i=1; i<=n; i++){
+            System.out.print(i+" ");
+        }
+    }
+}
+//This Code is Contributed by Vivek Kumar Gupta
