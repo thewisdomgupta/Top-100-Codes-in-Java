1ee04db2d4e19bf660a491726768abc187565195
Vivek Kumar Gupta <100516335+thewisdomgupta@users.noreply.github.com>
2022-04-05 15:19:17 +0530

    Add files via upload

---
+++ Array/ArrayPrinting.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class ArrayPrinting {
+    public static void main(String[] args){
+        int [] intArray;  //Array Declaration
+        intArray=new int[5]; //Array Instantiation
+        intArray[0]=1; //Array Initialization
+        intArray[1]=2;
+        intArray[2]=3;
+        intArray[3]=4;
+        intArray[4]=5;
+        System.out.println("Array is:");
+        System.out.println(Arrays.toString(intArray));
+    }
+}
+//This Code is Contributed by Vivek Kumar Gupta
