22b468a321966c2593820ef4cd9fb09c5f11a202
Vivek Kumar Gupta <100516335+thewisdomgupta@users.noreply.github.com>
2022-04-07 01:42:04 +0530

    Add files via upload

---
+++ Array/Copy_Array.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class Copy_Array {
+    public static void main(String [] args){
+        int [] array_First={45,56,98,52,92};
+        int [] array_Second;
+        array_Second=array_First;
+        System.out.println("Elements in First Array are:");
+        System.out.println(Arrays.toString(array_First));
+        System.out.println("After Copying Elements in Second Array are:");
+        System.out.println(Arrays.toString(array_Second));
+    }
+}
+//This Code is Contributed by Vivek Kumar Gupta
