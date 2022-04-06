f7b29d89e3de8d78da0816f9ed2ae0a81a43877b
Vivek Kumar Gupta <100516335+thewisdomgupta@users.noreply.github.com>
2022-04-06 10:07:24 +0530

    Add files via upload

---
+++ Array/MaxProduct.java
@@ -0,0 +1,20 @@
+public class MaxProduct {
+    public static void main(String [] args){
+        int [] arr={20,30,40,50,60,70,80};
+        int maxProduct=0;
+        String pairs=" ";
+        for(int i=0; i< arr.length; i++){
+            for(int j=i+1; j< arr.length; j++){
+                int product=arr[i]*arr[j];
+                if(product>maxProduct){
+                    maxProduct=product;
+                    pairs=Integer.toString(arr[i])+","+Integer.toString(arr[j]);
+                }
+            }
+        }
+        System.out.println("Greatest Pairs in the Array are:");
+        System.out.println(pairs);
+        System.out.println("Product of the Greatest Pairs in the Array is:");
+        System.out.println(maxProduct);
+    }
+}
