8865f8eaf47c82bbf83055b89c1f097fb99358e4
Vivek Kumar Gupta <100516335+thewisdomgupta@users.noreply.github.com>
2022-04-07 07:57:13 +0530

    Add files via upload

---
+++ Sorting/Selection_Sort.java
@@ -0,0 +1,36 @@
+//Selection Sort in Java
+import java.util.Arrays;
+import java.util.Scanner;
+public class Selection_Sort {
+    public static void main(String[] args) {
+        System.out.println("Enter Length of the Array:");
+        Scanner vivek = new Scanner(System.in);
+        int n = vivek.nextInt();
+        System.out.println("Enter Array Elements:");
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = vivek.nextInt();
+        }
+        System.out.println("Entered Array Elements before Sorting is:");
+        System.out.println(Arrays.toString(arr));
+        //Selection Sort
+        for(int j=0; j<n; j++){
+            int minimumIndex=j;
+            for(int i=j+1; i<n; i++ ){
+                if(arr[i]<arr[minimumIndex]){
+                    minimumIndex=i;
+                }
+            }
+            if(minimumIndex!=j){
+                int temp=arr[j];
+                arr[j]=arr[minimumIndex];
+                arr[minimumIndex]=temp;
+            }
+        }
+        System.out.println("Array Elements after Selection Sorting is:");
+        for(int i=0; i<n; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
+//This Code is Contributed by Vivek Kumar Gupta
